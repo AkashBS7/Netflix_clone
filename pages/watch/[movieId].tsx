868d0382bc75b7d6ead6bf6b1898b9f275f4c834
Akash Behara <akashbehara33@gmail.com>
2023-04-06 18:21:01 +0530

    video player

---
+++ pages/watch/[movieId].tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {useRouter} from 'next/router';
+import useMovie from '@/hooks/useMovie';
+
+import {AiOutlineArrowLeft} from 'react-icons/ai';
+
+const Watch = () => {
+  const router = useRouter();
+  const {movieId} = router.query;
+
+  const {data} = useMovie(movieId as string); 
+  return (
+    <div className='h-screen w-screen bg-black'>
+      <nav className='fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70'>
+        <AiOutlineArrowLeft onClick={() => {router.push('/')}} className='text-white cursor-pointer' size={40}/>
+        <p className='text-white text-1xl lg:text-3xl font-bold'>
+          <span className="font-light mr-2">
+            Watching: 
+          </span>
+          {data?.title}
+        </p>
+      </nav>
+      <video src={data?.videoUrl} autoPlay controls className='h-full w-full'></video>
+    </div>
+  )
+}
+
+export default Watch
